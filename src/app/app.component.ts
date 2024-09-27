c9f99c7f799b30628725c06449e379a3e112c521
Chakri Nandi <nandi.chakrapani@gmail.com>
2024-09-27 19:54:43 +0530

    Folder Structure is Created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReservationComponent } from "./reservation-form/reservation-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReservationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
+import { HomeComponent } from './home/home.component';
+import { ReservationComponent as ReservationFormComponent } from './reservation-form/reservation-form.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Home',
+        //loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+        component:HomeComponent,
+    },
+    {
+        path:'list', 
+        title:'Reservation List',
+        component:ReservationListComponent,
+    },
+    {
+        path:'new', title:'New Reservation',component:ReservationFormComponent,
+    },
+    { path: '**', 
+      title: 'Page Not Found',
+      component: PageNotFoundComponent,  
+     }
+];
